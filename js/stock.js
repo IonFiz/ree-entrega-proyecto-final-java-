fb987b5774af5bcd5800b1b830f578a35792c0fa
adolfo nin <ionfiz92@gmail.com>
2022-09-10 18:50:51 -0300

    re entrega java scrio adolfo nin

---
+++ js/stock.js
@@ -0,0 +1,21 @@
+let stockProductos = [
+    {id: 1, freeshipping: false, nombre: "CDJ 2000nx2", tipo: "CDJ PIONEER", desc: "Standard DJ Equipment", precio: 9000,  img: './assets/images/nexus2000_2.jpg'},
+    {id: 2, freeshipping: true, nombre: "CDJ 2000nx", tipo: "CDJ PIONEER", desc: "Standard DJ Equipment", precio: 8000,  img: './assets/images/nexus2000.jpg'},
+    {id: 3, freeshipping: false, nombre: "Xone 92", tipo: "Allen & Heath Mixer", desc: "Standard DJ Equipment", precio: 7000, img: './assets/images/xone92.jpg'},
+    {id: 4, freeshipping: false, nombre: "DJM 900nx2", tipo: "PIONEER Mixer", desc: "Standard DJ Equipment", precio: 7000,  img: './assets/images/djm900nexus2.jpg'},
+    {id: 5, freeshipping: true, nombre: "Sl 1200 MKIII", tipo: "Technics Turntables", desc: "Classic Old School DJ Equipment", precio: 8000,  img: './assets/images/sl1200mk3.jpg'},
+    {id: 6, freeshipping: false, nombre: "Das Vantec Combo", tipo: "Parlantes Vantec", desc: "High Quality DJ Sound Equipment", precio: 2200,  img: './assets/images//vantecpack.jpg'},
+    {id: 7, freeshipping: true, nombre: "Das Vantec 215", tipo: "Parlantes Vantec", desc: "High Quality DJ Sound Equipment", precio: 17000,  img: './assets/images/ev15ekx.jpg'},
+    {id: 8, freeshipping: true, nombre: "Funktion-one Resolution", tipo: "Parlantes FUNKTION 1", desc: "Premium DJ Equipment", precio: 40000,  img: './assets/images/funktionone_revo2.jpg'},
+    {id: 9, freeshipping: true, nombre: "CABINA NXS 1", tipo: "CDJ 2000 Nexus 1 x 2u Mixer Xone 92 o Pioneer DJM 900 Nexus", desc: "Standard DJ Equipment Combo", precio: 13000,  img: './assets/images/cabina-standard.png'},
+    {id: 10, freeshipping: true, nombre: "CABINA NXS 2", tipo: "CDJ 2000 Nexus 2 x 2u Mixer Pioneer DJM 900 Nexus 2", desc: "Standard DJ Equipment Combo", precio: 14000,  img: './assets/images/cabina-standard.png'},
+    {id: 11, freeshipping: true, nombre: "CABINA FULL", tipo: "CDJ 2000 Nexus (* extra Nexus 2 $2.000) x 2u Technics 1200MK3 x 2u Mixer Xone o Pioneer", desc: "Hybrid DJ Equipment Combo", precio: 21000,  img: './assets/images/scabina-full.png'},
+    {id: 12, freeshipping: true, nombre: "CABINA FULL PRO", tipo: "CDJ 2000 Nexus (* extra Nexus 2 $2.000) x 2u Mixer Xone o Pioneer EV 15 + 18 o FBT 15 + 18", desc: "Standard DJ Equipment Combo + High Quality DJ Sound Equipment ", precio: 26000,  img: './assets/images/cabina-fullpro.png'},
+    {id: 13, freeshipping: true, nombre: "COMBO X2", tipo: "2 FBT XPro15 o 2 EKX15 Sonido para 20-50 personas", desc: "High Quality DJ Sound Equipment", precio: 8500,  img: './assets/images/sonido-combo_x2.png'},
+    {id: 14, freeshipping: true, nombre: "COMBO SUB", tipo: "2 FBT XPro15 + 1 FBT X sub 18SA o 2 EKX15 + EKX18 Sonido para 30-100 personas", desc: "High Quality DJ Sound Equipment", precio: 13000,  img: './assets/images/sonido-combo_standard.png'},
+    {id: 15, freeshipping: true, nombre: "COMBO X4", tipo: "4 FBT XPro15 o 4 EKX15 Sonido para 50-150 personas", desc: "High Quality DJ Sound Equipment", precio: 13000,  img: './assets/images/sonido-combo_x4.png'},
+    {id: 16, freeshipping: true, nombre: "FUNKTION ONE Stage Booth", tipo: "Funktion one Stage Booth sonido para 200-250 personas", desc: "Premium DJ Sound Equipment", precio: 70000,  img: './assets/images/funktionone_218.jpg'},
+    
+    
+    
+]
